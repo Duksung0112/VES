308e9288220710c67ad4a8143470089fc57add8f
Juna <wja0810@naver.com>
2021-02-10 23:42:03 +0900

    many many

---
+++ app/src/main/java/com/example/ves/ModifyActivity.java
@@ -0,0 +1,13 @@
+package com.example.ves;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ModifyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.modify);
+    }
+}
