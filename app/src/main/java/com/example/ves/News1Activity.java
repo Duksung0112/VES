38302b79cb72c430de7c739dbf5c1651de9bcacd
Juna <wja0810@naver.com>
2021-05-12 20:20:50 +0900

    many many

--- app/src/main/java/com/example/ves/News1Activity.java
+++ app/src/main/java/com/example/ves/News1Activity.java
@@ -1,6 +1,9 @@
 package com.example.ves;
 
+import android.Manifest;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
@@ -9,13 +12,18 @@ import android.text.Spanned;
 import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.method.ScrollingMovementMethod;
+import android.text.style.BackgroundColorSpan;
 import android.text.style.ClickableSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -25,12 +33,24 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 
 public class News1Activity extends Fragment {
     TextView lv;
+    Intent intent;
+    SpeechRecognizer mRecognizer;
+    ImageButton voiceRecord;
+    TextView sttResult;
+    final int PERMISSION = 1;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,50 +59,27 @@ public class News1Activity extends Fragment {
 
         lv= (TextView) view.findViewById(R.id.lv);
         TextView newscontent = (TextView) view.findViewById(R.id.newscontent);
+        TextView question = (TextView) view.findViewById(R.id.question);
 
         newscontent.setMovementMethod(new ScrollingMovementMethod());
 
 
-
-
-
+/*
 
         Spannable span = Spannable.Factory.getInstance().newSpannable("click");
         String text = span.toString();
 
         int start = text.indexOf("click");
         int end = start + "click".length();
-        span.setSpan(new ClickableSpan() {
-            @Override
-            public void onClick(View widget) {
-
-            }
-
-            @Override
-            public void updateDrawState(TextPaint ds) {
-                ds.linkColor = 00000;
-                super.updateDrawState(ds);
-            }
-        }, start, end, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        span.setSpan(new UnderlineSpan()
+                , start, end, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
 
         newscontent.setText(span);
         newscontent.setHighlightColor(Color.TRANSPARENT);
         newscontent.setMovementMethod(LinkMovementMethod.getInstance());
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
         String url = "https://www.theguardian.com/international";
@@ -94,9 +91,10 @@ public class News1Activity extends Fragment {
         }
         Elements element = doc.select("div.fc-container__inner");
 
-        Element el = element.select("h3.fc-item__title").first();
+        Elements el = element.select("h3.fc-item__title");
+        Element el1 = el.get(0);
 
-        String url2 = el.select("a").attr("href");
+        String url2 = el1.select("a").attr("href");
 
         Document doc2 = null;
         try {
@@ -105,11 +103,100 @@ public class News1Activity extends Fragment {
             e.printStackTrace();
         }
 
-        Elements element2 = doc2.select("div.css-a6edhj");
+        Elements element2 = doc2.select("div.css-8bpe6e");
         System.out.println(element2.text());
         newscontent.setText(element2.text());
 
+        List<String> qValues = new ArrayList<>();
 
+        qValues.add("Q : How was the article?");
+        qValues.add("Q : What is the most important point in this article?");
+        qValues.add("Q : What did you get from this article?");
+        qValues.add("Q : What do you think about this topic?");
+        qValues.add("Q : What did you feel in this article?");
+
+        Random r = new Random();
+        String randomValue = qValues.get(r.nextInt(qValues.size()));
+        question.setText(randomValue);
+
+        if ( Build.VERSION.SDK_INT >= 23 ){
+            // 퍼미션 체크
+            ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.INTERNET,
+                    Manifest.permission.RECORD_AUDIO},PERMISSION);
+        }
+
+        sttResult = (TextView)view.findViewById(R.id.sttResult);
+        voiceRecord = (ImageButton)view.findViewById(R.id.voiceRecord);
+        intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getActivity().getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
+        voiceRecord.setOnClickListener(v -> {
+            mRecognizer=SpeechRecognizer.createSpeechRecognizer(this.getActivity());
+            mRecognizer.setRecognitionListener(listener);
+            mRecognizer.startListening(intent);
+        });
         return view;
     }
+
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override public void onReadyForSpeech(Bundle params) {
+            Toast.makeText(getActivity().getApplicationContext(),"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+        }
+        @Override public void onBeginningOfSpeech() {}
+        @Override public void onRmsChanged(float rmsdB) {}
+        @Override public void onBufferReceived(byte[] buffer) {}
+        @Override public void onEndOfSpeech() {}
+        @Override public void onError(int error) {
+            String message;
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                    default: message = "알 수 없는 오류임";
+                    break;
+            }
+
+            Toast.makeText(getActivity().getApplicationContext(), "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for(int i = 0; i < matches.size() ; i++){
+                sttResult.setText(matches.get(i));
+            }
+        }
+        @Override
+        public void onPartialResults(Bundle partialResults) {}
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {} };
+
+
+
 }
--- app/src/main/java/com/example/ves/News3Activity.java
+++ app/src/main/java/com/example/ves/News3Activity.java
@@ -6,6 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import androidx.fragment.app.Fragment;
 
 public class News3Activity extends Fragment {
@@ -18,6 +28,45 @@ public class News3Activity extends Fragment {
         View view = inflater.inflate(R.layout.news3, container, false);
 
         lv= (TextView) view.findViewById(R.id.lv);
+        TextView newscontent = (TextView) view.findViewById(R.id.newscontent);
+        TextView question = (TextView) view.findViewById(R.id.question);
+
+        String url = "https://www.theguardian.com/international";
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements element = doc.select("div.fc-container__inner");
+
+        Elements el = element.select("h3.fc-item__title");
+        Element el3 = el.get(2);
+
+        String url2 = el3.select("a").attr("href");
+
+        Document doc2 = null;
+        try {
+            doc2 = Jsoup.connect(url2).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Elements element2 = doc2.select("div.css-8bpe6e");
+        System.out.println(element2.text());
+        newscontent.setText(element2.text());
+
+        List<String> qValues = new ArrayList<>();
+
+        qValues.add("Q : How was the article?");
+        qValues.add("Q : What do you think is the most important point in this article?");
+        qValues.add("Q : What did you get from this article?");
+        qValues.add("Q : What do you think about this topic?");
+        qValues.add("Q : What did you feel in this article?");
+
+        Random r = new Random();
+        String randomValue = qValues.get(r.nextInt(qValues.size()));
+        question.setText(randomValue);
 
         return view;
     }
